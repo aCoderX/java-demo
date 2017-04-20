da6071d3dee745a04db137e0eca551405bd14f63
xudi <1121535770@qq.com>
2017-04-20 23:46:10 +0800

    原子操作

--- jdk/src/main/java/com/acoderx/demo/jdk/atomic/Atomic.java
+++ jdk/src/main/java/com/acoderx/demo/jdk/atomic/Atomic.java
@@ -2,39 +2,34 @@ package com.acoderx.demo.jdk.atomic;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Created by xudi on 17-4-13.
+ * Created by xudi on 17-4-20.
  */
-public class UnAtomic2 {
-    private static int num=0;
+public class Atomic {
+    private static AtomicInteger num=new AtomicInteger(0);
     private static long stime = System.currentTimeMillis();
     public static void main(String args[]){
-        ExecutorService services = Executors.newCachedThreadPool();
+        ExecutorService service = Executors.newCachedThreadPool();
         for(int i=0;i<30;i++){
-            services.execute(()->{
+            service.execute(()->{
                 int count = 1;
                 while (count<1000000){
                     count++;
-                    nextSave();
+                    next();
                 }
-                nextSave();
-                //System.out.println(nextSave());
+                next();
+                //System.out.println(next());
             });
         }
-        services.shutdown();
+        service.shutdown();
     }
     public static int next(){
-        num=num+1;
-        return num;
-    }
-
-    public synchronized static int nextSave(){
-        num=num+1;
-        if(num==30000000){
+        int res = num.incrementAndGet();
+        if(res==30000000){
             System.out.println("time"+(System.currentTimeMillis()-stime));
         }
-        return num;
+        return res;
     }
-
 }
